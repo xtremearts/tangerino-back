b358311f17438a44bae9be4530d8021cf1afcedf
Tiago <tiago.pereira@sigma.com.br>
2023-11-14 20:38:52 -0300

    primeiro commit

---
+++ src/main/java/br/com/tangerino/tangerino/TangerinoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tangerino.tangerino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangerinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TangerinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tangerino/tangerino/TangerinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tangerino.tangerino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangerinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
