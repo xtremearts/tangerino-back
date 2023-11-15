b3b378a603177e2fd735c92c26b325af66214b0e
Tiago <tiago.pereira@sigma.com.br>
2023-11-15 10:33:35 -0300

    configurando exibição no consoler

--- src/main/java/br/com/tangerino/tangerino/TangerinoApplication.java
+++ src/main/java/br/com/tangerino/tangerino/TangerinoApplication.java
@@ -1,13 +1,26 @@
 package br.com.tangerino.tangerino;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class TangerinoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TangerinoApplication.class, args);
-	}
+    private static final String SERVER_PORT = "server.port";
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(TangerinoApplication.class, args);
+
+
+        Environment env = ctx.getEnvironment();
+
+        log.info("\n\n *** \n\n" + "\tAplicacao iniciada com sucesso!\n" + "\tDisponivel no endereco:\n"
+                        + "\tLocal: http://localhost:{}\n",
+                env.getProperty(SERVER_PORT));
+    }
 
 }
